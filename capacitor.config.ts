7e1e3982b20046f564aa172084ce8559fa9ed624
VidMM <104316628+ElVidMM@users.noreply.github.com>
2022-08-28 23:23:17 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'conecta4',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
