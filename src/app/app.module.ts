9ae74f35b290a1b3d4995e1bd1c40239e6e48460
VidMM <104316628+ElVidMM@users.noreply.github.com>
2022-09-06 21:55:36 -0300

    Implement screen orientation plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx'
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ScreenOrientation
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/page/game/game.page.ts
+++ src/app/page/game/game.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamePage implements OnInit {
 
-  constructor() { }
-  
+  constructor(private screenOrientation: ScreenOrientation) {
+    console.log(this.screenOrientation.type);
+    
+  }
+
   ngOnInit() {
   }
+
+  
 
 }
